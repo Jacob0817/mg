00d8771b783e6f22e384377cbc9ce925226e2d8a
Jacob Zhou <zzh839985914@gmail.com>
2019-02-11 19:20:45 +0800

    设定公用静态资源文件夹

--- mg/settings.py
+++ mg/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +27,6 @@ SECRET_KEY = 'hrw&l=qzfij*p)kov9!zsgq*d2ifk16#rgfb(r=%ot^w4w=4ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -121,4 +123,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/statics/'
+STATIC_URL = '/static/'
--- mg/urls.py
+++ mg/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from MgForSchool import views as MgS_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('accounts/', include('users.urls')),
     path('', MgS_views.index, name='MgSindex'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
