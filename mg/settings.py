57b7864c04464f3ea0b65d539a3cadda67cd4f0b
Jacob Zhou <zzh839985914@gmail.com>
2019-02-27 00:56:44 +0800

    在账户模块的base模板中加入网站logo

--- mg/settings.py
+++ mg/settings.py
@@ -15,7 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, "MgForSchoolV/dist/"),
+    
 )
 
 
