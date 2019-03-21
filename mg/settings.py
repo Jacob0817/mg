ed2748df783e5b923e918b25fd5ec2f548a7d2f5
kirahan <hanzhaoapply@gmail.com>
2019-03-21 17:16:59 +0800

    fix bug , setting static content

--- mg/settings.py
+++ mg/settings.py
@@ -14,10 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, "MgForSchoolV/dist/"),
-    
 )
 
 
